d7a81c749b6728424159bed510b374018a0ca855
karolarizag <karolarizadev@gmail.com>
2022-09-05 16:11:42 +0100

    re-styled characters page

--- src/Components/CharactersCard/CharactersCard.tsx
+++ src/Components/CharactersCard/CharactersCard.tsx
@@ -7,8 +7,8 @@ export const Card = ({props}:any) => {
     <Link to={`/characters/${props.id}`}>
       <div className="card m-3 mx-2 image-full">
         <figure><img src={props.image} alt={props.name} /></figure>
-        <div className="p-5 flex justify-center">
-          <h2 className="card-title">
+        <div className="p-5 flex justify-center items-end">
+          <h2 className="card-title glass px-2 rounded">
             {props.name}
           </h2>
         </div>
--- src/Components/LocationsCard/LocationsCard.tsx
+++ src/Components/LocationsCard/LocationsCard.tsx
@@ -7,7 +7,7 @@ export const LocationsCard = ({props}:any) => {
         <h2 className="card-title">{props.name}</h2>
         <p>{props.type}, {props.dimension}</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-primary">{props.residents.length} residents</div>
+          <div className="badge badge-primary">{props.residents.length + 1} residents</div>
         </div>
       </div>
     </div>
